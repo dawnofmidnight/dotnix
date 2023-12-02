cc04f128fb4b5687f3bf06381916fe5c76f328f6
dawnofmidnight <dawnofmidnight@duck.com>
2023-12-01 23:37:29 -0500

    initial commit

---
+++ home.nix
@@ -0,0 +1,74 @@
+{ lib, pkgs, ... }: {
+  home = {
+    username = "dawn";
+    homeDirectory = "/home/dawn";
+    stateVersion = "23.05";
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.atuin = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+  
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "dawnofmidnight";
+    userEmail = "dawnofmidnight@duck.com";
+    ignores = [ "/.direnv/" ".envrc" ];
+    extraConfig = {
+      core.editor = "${lib.getExe pkgs.helix}";
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.gh = {
+    enable = true;
+    settings.git_protocol = "ssh";
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "github_light";
+      editor = {
+        line-number = "relative";
+        true-color = true;
+        auto-save = true;
+        rulers = [80];
+        bufferline = "always";
+        color-modes = true;
+        lsp = {
+          display-messages = false;
+          display-inlay-hints = true;
+        };
+        cursor-shape = {
+          normal = "bar";
+          insert = "bar";
+          select = "bar";
+        };
+      };
+    };
+  };
+
+  programs.nushell = {
+    enable = true;
+    configFile.source = ./nushell/config.nu;
+    envFile.source = ./nushell/env.nu;
+  };
+
+  programs.ripgrep.enable = true;
+
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+  
+  home.packages = with pkgs; [ tokei typst typst-lsp ];
+}
